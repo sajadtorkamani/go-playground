74260c49d96f45407f59a168ca65e1a841750c0a
Sajad Torkamani <sajadtorkamani1@gmail.com>
2024-08-17 14:17:17 +0100

    wip

--- high_and_low.go
+++ high_and_low.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func HighAndLow(str string) string {
+	chars := strings.Split(str, " ")
+	var nums []int
+
+	for _, char := range chars {
+		num, err := strconv.Atoi(char)
+
+		if err != nil {
+			panic(err)
+		}
+
+		nums = append(nums, num)
+	}
+
+	sort.Ints(nums)
+
+	return fmt.Sprintf("%d %d", nums[len(nums)-1], nums[0])
+}
+
+func main() {
+	fmt.Println(HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+}
--- http-server.go
+++ http-server.go
@@ -12,8 +12,9 @@ type Quote struct {
 }
 
 var quotes = []Quote{
-	Quote{Quote: "We're all going to die", Author: "Sajad"},
-	Quote{Quote: "Life is absurd", Author: "Sajad"},
+	Quote{Quote: "Quote 1", Author: "Sajad"},
+	Quote{Quote: "Quote 2", Author: "Sajad"},
+	Quote{Quote: "Quote 3", Author: "Sajad"},
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -24,14 +25,29 @@ func listQuotes(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(quotes)
 }
 
+func createQuote(w http.ResponseWriter, r *http.Request) {
+	response := struct {
+		Message string
+	}{
+		Message: "Ok!",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/quotes", listQuotes)
+	http.HandleFunc("GET /quotes", listQuotes)
+	http.HandleFunc("POST /quotes", createQuote)
 }
 
 func main() {
 	PORT := 8080
 	fmt.Printf("Server listening on http://localhost:%v\n", PORT)
 	handleRequests()
-	http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
+	if err != nil {
+		panic(err)
+	}
 }
