fa37550e164438d7be969f5ff67fedac2453574f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2024-08-17 13:14:27 +0100

    wip

--- if/go.mod
+++
@@ -1,3 +0,0 @@
-module example.com/if
-
-go 1.16
--- pointers.go
+++ pointers.go
@@ -15,17 +15,4 @@ func main() {
 
 	fmt.Println(myPointer)
 	fmt.Println(*myPointer)
-
-	&myPointer = 100
-
-	fmt.Println(num)
-
-	var myInt = 42
-	var myIntPointer = &myInt
-
-	fmt.Println(*myIntPointer) // prints 42
-
-	&myIntPointer = 21 // sets the value myInt to 42 through the myIntPointer
-
-	fmt.Println(myInt) // prints 21
 }
