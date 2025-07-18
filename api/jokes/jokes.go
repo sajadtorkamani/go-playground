44d8853f4b270152c958f0876fffe1f9a113237b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-07-18 23:13:12 +0100

    wip

---
+++ api/jokes/jokes.go
@@ -0,0 +1,26 @@
+package jokes
+
+import (
+	"go-playground/api/db"
+	"go-playground/api/models"
+)
+
+func JokeDoesNotExist(jokeContent string) bool {
+	var joke models.Joke
+
+	db.DB.Where("content = ?", jokeContent).First(&joke)
+
+	return joke.ID == 0
+}
+
+func GetJokeCount() int64 {
+	var totalNumJokes int64
+
+	result := db.DB.Model(&models.Joke{}).Count(&totalNumJokes)
+
+	if result.Error != nil {
+		panic(result.Error)
+	}
+
+	return totalNumJokes
+}
---
+++ api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go-playground/api/db"
+	"go-playground/api/jokes"
+	"go-playground/api/models"
+)
+
+func main() {
+	db.ConnectToDatabase()
+
+	JOKES := []string{"Joke 1", "Joke 2", "Joke 3", "Joke 4"}
+	numJokesAdded := 0
+
+	for _, joke := range JOKES {
+		if jokes.JokeDoesNotExist(joke) {
+			db.DB.Create(&models.Joke{Content: joke})
+			numJokesAdded += 1
+		}
+	}
+
+	if numJokesAdded > 0 {
+		fmt.Println(
+			fmt.Sprintf("✅  Added new %d jokes. We now have %d jokes in total.", numJokesAdded, jokes.GetJokeCount()),
+		)
+	} else {
+		fmt.Println("No jokes added.")
+	}
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello!",
+		})
+	})
+
+	router.GET("/jokes", func(c *gin.Context) {
+		c.JSON(200, gin.H{"jokeCount": jokes.GetJokeCount()})
+	})
+
+	fmt.Println("🚀 Server listening on http://localhost:8080")
+
+	router.Run()
+}
